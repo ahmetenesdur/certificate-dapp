88b29d165479b5a29d735f77e766cabc0e08bd01
Enes <70810472+ahmetenesdur@users.noreply.github.com>
2023-01-18 21:29:06 +0300

    Comment Line Added to Utils

--- utils/saveToIPFS.js
+++ utils/saveToIPFS.js
@@ -1,21 +1,26 @@
 import axios from "axios";
 
 const saveToIPFS = async (file) => {
-    const formData = new FormData();
-    formData.append("file", file);
+  // Create a new FormData instance to store the file
+  const formData = new FormData();
+  // Append the file to the form data
+  formData.append("file", file);
 
-    const config = {
-        method: "post",
-        url: "https://api.web3.storage/upload",
-        headers: {
-            Authorization: `Bearer ${process.env.NEXT_PUBLIC_WEB3_STORAGE_TOKEN}`,
-            "Content-Type": "multipart/form-data",
-        },
-        data: formData,
-    };
+  // Configure the axios request
+  const config = {
+    method: "post",
+    url: "https://api.web3.storage/upload",
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_WEB3_STORAGE_TOKEN}`,
+      "Content-Type": "multipart/form-data",
+    },
+    data: formData,
+  };
 
-    const response = await axios(config);
-    return response.data.cid;
+  // Send the request to the IPFS API
+  const response = await axios(config);
+  // Return the CID of the file
+  return response.data.cid;
 };
 
 export default saveToIPFS;
